001287b926ad1dc4ae26e7e4c2a2a6a504ea023f
Rodrigo <rod_novaes@hotmail.com>
2024-11-20 12:31:06 +0100

    Create the Home component to display the podcast cards

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import PodcastCard from "../components/PodcastCard/PodcastCard";
+import { usePodcasts } from "../Hooks/usePodcasts";
+import styles from "./Home.module.scss";
+import SearchBar from "../components/SearchBar/SearchaBar";
+
+const Home = () => {
+  const { podcasts, loading } = usePodcasts();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredPodcasts = podcasts.filter((podcast) =>
+    podcast["im:name"].label.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className={styles.home}>
+      <div className={styles.home__search}>
+        <span className={styles.home__search__count}>
+          {filteredPodcasts.length}
+        </span>
+        <SearchBar onSearch={setSearchTerm} value={searchTerm} />
+      </div>
+
+      {loading ? (
+        <div className={styles.home__loading}>Loading...</div>
+      ) : (
+        <div className={styles.home__podcasts}>
+          {filteredPodcasts.map((podcast) => (
+            <PodcastCard
+              key={podcast.id.attributes["im:id"]}
+              podcast={podcast}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
