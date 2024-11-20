6ce894b56f63694ad40f9793511ec364e9a52845
Rodrigo <rod_novaes@hotmail.com>
2024-11-20 19:59:16 +0100

    Create the EpisodeList component

---
+++ src/components/EpisodeList/EpisodeList.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import { convertMillisecondsToTime } from "../../utils/convertMillisecondsToTime";
+import { formatDate } from "../../utils/formatDate";
+import styles from "./EpisodeList.module.scss";
+import Spinner from "../Spinner/Spinner";
+
+const EpisodeList = ({ podcastId, tracks, loading }) => {
+  if (loading) return <Spinner />;
+
+  return (
+    <div className={styles.episodeList}>
+      <h2 className={styles.episodeList__header}>Episodes: {tracks.length}</h2>
+
+      <div className={styles.episodeList__table}>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Date</th>
+              <th>Duration</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tracks.map((episode) => (
+              <tr key={episode.id}>
+                <td>
+                  <Link to={`/podcast/${podcastId}/episode/${episode.trackId}`}>
+                    {episode.trackName}
+                  </Link>
+                </td>
+                <td>{formatDate(episode.releaseDate)}</td>
+                <td>{convertMillisecondsToTime(episode.trackTimeMillis)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default EpisodeList;
