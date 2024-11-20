0d3c3abcacdb4130d0902fae1b69c671c529750f
Rodrigo <rod_novaes@hotmail.com>
2024-11-20 13:16:19 +0100

    Fix some components imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Home from "./pages/Home";
-import PodcastDetail from "./pages/PodcastDetail";
+import Home from "./Pages/Home";
 import EpisodeDetail from "./pages/EpisodeDetail";
+import PodcastDetail from "./Pages/PodcastDetail";
 
 const App = () => {
   return (
