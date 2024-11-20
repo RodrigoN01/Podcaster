9712e42639e340fe316a86f851fa1bad9e00f67c
Rodrigo <rod_novaes@hotmail.com>
2024-11-20 13:19:00 +0100

    Create a custom hook to handle the data for the podcast details

---
+++ src/Hooks/usePodcastDetails.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { fetchPodcastDetails } from "../utils/fetchData";
+
+export const usePodcastDetails = (podcastId) => {
+  const [podcastDetails, setPodcastDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  console.log("here1", podcastId);
+
+  useEffect(() => {
+    const loadPodcast = async () => {
+      try {
+        const data = await fetchPodcastDetails(podcastId);
+        console.log("here", data);
+        setPodcastDetails(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadPodcast();
+  }, [podcastId]);
+
+  return { podcastDetails, loading, error };
+};
